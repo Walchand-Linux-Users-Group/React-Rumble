11bf77132bc68543e95189db64da44eb8afe0b07
ThunderSmoker <103036441+ThunderSmoker@users.noreply.github.com>
2023-04-29 11:05:25 +0530

    Update api.js

--- l2/src/API/api.js
+++ l2/src/API/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const API=axios.create({baseURL:'https://react-rumble.onrender.com'});
 const tryme=()=> API.get('/level2/somethingJustLikeThis');
-const iamreal=()=> API.get('/level2/maihuHiroTera');
-const yugotme=()=> API.get('/level2/okYouGotME');
+const iamreal=()=> API.get('/level2/maiHuHiroTera');
+const yugotme=()=> API.get('/level2/teraHoneLagaHu');
 
 export {tryme,iamreal,yugotme};
